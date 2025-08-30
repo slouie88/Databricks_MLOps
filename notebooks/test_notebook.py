a35731b797ba66b3188d263cceffb4d330a4560b
slouie88 <slouie88>
2025-08-30 06:36:58 +0000

    Testing of bringing in dependencies into databricks notebook

---
+++ notebooks/test_notebook.py
@@ -0,0 +1,9 @@
+# Databricks notebook source
+# %pip install -e ..
+# %restart_python
+
+# COMMAND ----------
+
+from pathlib import Path
+import sys
+sys.path.append(str(Path.cwd().parent / 'src'))
